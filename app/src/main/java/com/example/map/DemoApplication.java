6e2de7537f1b0b85c6cf07e6c8dc40030c4b2520
aka1i <847919557@qq.com>
2019-03-27 01:50:18 +0800

    完成定位到自己

--- app/src/main/java/com/example/map/DemoApplication.java
+++ app/src/main/java/com/example/map/DemoApplication.java
@@ -1,6 +1,7 @@
 package com.example.map;
 
 import android.app.Application;
+import android.graphics.Color;
 
 import com.baidu.mapapi.CoordType;
 import com.baidu.mapapi.SDKInitializer;
@@ -14,5 +15,8 @@ public class DemoApplication  extends Application {
         //自4.3.0起，百度地图SDK所有接口均支持百度坐标和国测局坐标，用此方法设置您使用的坐标类型.
         //包括BD09LL和GCJ02两种坐标，默认是BD09LL坐标。
         SDKInitializer.setCoordType(CoordType.BD09LL);
+
+
+
     }
 }
--- app/src/main/java/com/example/map/MainActivity.java
+++ app/src/main/java/com/example/map/MainActivity.java
@@ -1,16 +1,40 @@
 package com.example.map;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.graphics.Point;
+import android.os.Build;
+import android.service.notification.StatusBarNotification;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.transition.AutoTransition;
+import android.transition.TransitionManager;
+import android.transition.TransitionSet;
 import android.util.Log;
+import android.view.Display;
+import android.view.KeyEvent;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 import com.baidu.location.BDAbstractLocationListener;
 import com.baidu.location.BDLocation;
 import com.baidu.location.LocationClient;
 import com.baidu.location.LocationClientOption;
 import com.baidu.mapapi.map.BaiduMap;
+import com.baidu.mapapi.map.BaiduMapOptions;
 import com.baidu.mapapi.map.MapStatusUpdate;
 import com.baidu.mapapi.map.MapStatusUpdateFactory;
 import com.baidu.mapapi.map.MapView;
@@ -22,41 +46,31 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
     private MapView mMapView = null;
     private BaiduMap mBaiduMap;
     private Button mFindMeButton;
+    private EditText mSearch;
+    private CardView mSearchLayout;
+    private CardView mFindMeCardView;
     private LocationClient mLocationClient;
+    private RelativeLayout mMainRelativeLayout;
+    private TransitionSet mSet;
     private double mCurrentX;
     private double mCurrentY;
     private boolean isFirstLoad = true;
+    private String[] permissions = new String[]{
+            Manifest.permission.ACCESS_FINE_LOCATION,};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        mFindMeButton = findViewById(R.id.find_me_btn);
-        //获取地图控件引用
-        mMapView =  findViewById(R.id.bmapView);
-        mBaiduMap = mMapView.getMap();
-        mBaiduMap.setMyLocationEnabled(true);
-        //定位初始化
         mLocationClient = new LocationClient(this);
 
-//通过LocationClientOption设置LocationClient相关参数
-        LocationClientOption option = new LocationClientOption();
-        option.setOpenGps(true); // 打开gps
-        option.setCoorType("bd09ll"); // 设置坐标类型
-        option.setIsNeedAddress(true); //设置需要地址信息
-        option.setScanSpan(1000);
+        setContentView(R.layout.activity_main);
 
-//设置locationClientOption
-        mLocationClient.setLocOption(option);
+        //请求权限
+        requestPower();
 
-//注册LocationListener监听器
-        MyLocationListener myLocationListener = new MyLocationListener();
-        mLocationClient.registerLocationListener(myLocationListener);
-//开启地图定位图层
-        mLocationClient.start();
+        init();
 
 
-        mFindMeButton.setOnClickListener(this);
+
     }
     @Override
     protected void onResume() {
@@ -113,6 +127,63 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
         }
     }
 
+    private void init(){
+        mFindMeButton = findViewById(R.id.find_me_btn);
+        mSearch = findViewById(R.id.edittxt_search);
+        mSearchLayout = findViewById(R.id.ll_search);
+        mMainRelativeLayout = findViewById(R.id.main_rl);
+        mFindMeCardView = findViewById(R.id.find_me_cardview);
+        //获取地图控件引用
+        mMapView =  findViewById(R.id.bmapView);
+        mBaiduMap = mMapView.getMap();
+        mBaiduMap.setMyLocationEnabled(true);
+
+        mBaiduMap.setOnMapLoadedCallback(new BaiduMap.OnMapLoadedCallback() {
+            @Override
+            public void onMapLoaded() {
+                mMapView.setZoomControlsPosition(new Point(mFindMeCardView.getLeft(),mFindMeCardView.getTop() - 350)); //设置缩放控件位置
+            }
+        });
+        //定位初始化
+        //通过LocationClientOption设置LocationClient相关参数
+        LocationClientOption option = new LocationClientOption();
+        option.setLocationMode(LocationClientOption.LocationMode.Hight_Accuracy);
+        option.setOpenGps(true); // 打开gps
+        option.setCoorType("bd09ll"); // 设置坐标类型
+        option.setIsNeedAddress(true); //设置需要地址信息
+        option.setScanSpan(1000);
+//设置locationClientOption
+        mLocationClient.setLocOption(option);
+
+
+//注册LocationListener监听器
+        MyLocationListener myLocationListener = new MyLocationListener();
+        mLocationClient.registerLocationListener(myLocationListener);
+
+        mFindMeButton.setOnClickListener(this);
+
+        mSearch.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (hasFocus){
+                    expand();
+                }else {
+                    reduce();
+                }
+            }
+        });
+
+        mSearch.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                mSearch.setFocusable(true);
+                mSearch.setFocusableInTouchMode(true);
+                mSearch.requestFocus();
+                return false;
+            }
+        });
+    }
+
     @Override
     public void onClick(View v) {
         switch (v.getId()){
@@ -141,4 +212,134 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
         update = MapStatusUpdateFactory.zoomTo(19f);
         mBaiduMap.animateMapStatus(update);
     }
+
+    public void requestPower(){
+        if (ContextCompat.checkSelfPermission(MainActivity.this,
+                Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+
+                ActivityCompat.requestPermissions(this,
+                       permissions, 1);
+        }else {
+            requestLocation();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode){
+            case 1:
+                if (grantResults.length > 0){
+                    for (int result : grantResults){
+                        if (result != PackageManager.PERMISSION_GRANTED){
+                            Toast.makeText(this,"必须同意所有权限才能使用本程序",Toast.LENGTH_SHORT).show();
+                            finish();
+                            return;
+                        }
+                        requestLocation();
+                    }
+                }else {
+                    Toast.makeText(this,"发生未知错误",Toast.LENGTH_SHORT).show();
+                    finish();
+                }
+        }
+    }
+
+    private void requestLocation(){
+        //开启地图定位图层
+        mLocationClient.start();
+    }
+
+
+    private void expand() {
+        //设置伸展状态时的布局
+     //   mSearch.setText("搜索简书的内容和朋友");
+        RelativeLayout.LayoutParams LayoutParams = (RelativeLayout.LayoutParams) mSearchLayout.getLayoutParams();
+        LayoutParams.width = LayoutParams.MATCH_PARENT;
+        LayoutParams.setMargins(dip2px(10), dip2px(10), dip2px(10), dip2px(10));
+        mSearchLayout.setLayoutParams(LayoutParams);
+        //设置动画
+        beginDelayedTransition(mSearchLayout);
+    }
+
+    private void reduce() {
+        //设置收缩状态时的布局
+        //mSearch.setText("搜索");
+        RelativeLayout.LayoutParams LayoutParams = (RelativeLayout.LayoutParams) mSearchLayout.getLayoutParams();
+        LayoutParams.width = dip2px(80);
+        LayoutParams.setMargins(dip2px(10), dip2px(10), dip2px(10), dip2px(10));
+        mSearchLayout.setLayoutParams(LayoutParams);
+        //设置动画
+        beginDelayedTransition(mSearchLayout);
+    }
+
+    void beginDelayedTransition(ViewGroup view) {
+        mSet = new AutoTransition();
+        //设置动画持续时间
+        mSet.setDuration(300);
+        // 开始表演
+        TransitionManager.beginDelayedTransition(view, mSet);
+    }
+
+    private int dip2px(float dpVale) {
+        final float scale = getResources().getDisplayMetrics().density;
+        return (int) (dpVale * scale + 0.5f);
+    }
+
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent ev) {
+        if (ev.getAction() == MotionEvent.ACTION_DOWN) {
+            View v =  getCurrentFocus();
+            Log.d(TAG, "v: " + v);
+            if (isShouldHideInput(v, ev)) {
+                mSearch.setFocusable(false);
+                InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                if (imm != null) {
+                    imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
+                }
+            }
+            else if ((v instanceof EditText)){
+                InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                if (imm != null) {
+                    v.requestFocus();
+                    imm.showSoftInput(v, 0);
+                }
+            }
+            return super.dispatchTouchEvent(ev);
+        }
+        // 必不可少，否则所有的组件都不会有TouchEvent了
+        if (getWindow().superDispatchTouchEvent(ev)) {
+            return true;
+        }
+        return onTouchEvent(ev);
+    }
+
+    public  boolean isShouldHideInput(View v, MotionEvent event) {
+        if (v != null && (v instanceof EditText)) {
+            int[] leftTop = {0, 0};
+            //获取输入框当前的location位置
+            ((ViewGroup)v.getParent()).getLocationInWindow(leftTop);
+            int left = leftTop[0];
+            int top = leftTop[1];
+
+            int bottom = ((ViewGroup)v.getParent()).getHeight() + top;
+            int right = ((ViewGroup)v.getParent()).getWidth() + left;
+            Log.d("touchEventP", "left: " +left );
+            Log.d("touchEventP", "top: " + top);
+            Log.d("touchEventP", "bottom: " + bottom);
+            Log.d("touchEventP", "right: " + right);
+            Log.d("touchEventP", "event.getX(): " + event.getX());
+            Log.d("touchEventP", "event.getY(): " + event.getY());
+
+            if (event.getX() > left && event.getX() < right
+                    && event.getY() > top && event.getY() < bottom) {
+                // 点击的是输入框区域，保留点击EditText的事件
+                return false;
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
+
+
 }
